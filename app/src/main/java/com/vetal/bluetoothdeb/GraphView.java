babc92af87405d21ff37773fc44216de8b47eefa
Vitali Ermisco <Vermisco@gmail.com>
2019-06-14 22:22:53 +0300

    13.06.2019

---
+++ app/src/main/java/com/vetal/bluetoothdeb/GraphView.java
@@ -0,0 +1,121 @@
+package com.vetal.bluetoothdeb;
+
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
+import org.achartengine.chart.BarChart;
+import org.achartengine.model.SeriesSelection;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.model.XYSeries;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
+import org.achartengine.renderer.XYSeriesRenderer;
+
+
+public class GraphView extends MainActivity {
+
+    public GraphicalView mChart;
+    private XYMultipleSeriesDataset mDataset = new XYMultipleSeriesDataset();
+    private XYMultipleSeriesRenderer mRenderer = new XYMultipleSeriesRenderer();
+    private XYSeries mCurrentSeries;
+    LinearLayout layout;
+    Button BtnGr;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.graph_view);
+        setup();
+    }
+
+    protected void setup() {
+        layout = (LinearLayout) findViewById(R.id.LinearLayout);
+        BtnGr = (Button) findViewById(R.id.Back);
+        if (mChart == null) {
+            initChart();
+            addSampleData();
+            mChart = ChartFactory.getBarChartView(this, mDataset, mRenderer, BarChart.Type.DEFAULT);
+            layout.addView(mChart);
+        } else {
+            mChart.repaint();
+        }
+        mChart.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(GraphView.this, "ON CLICK", Toast.LENGTH_SHORT).show();
+                SeriesSelection seriesSelection = mChart.getCurrentSeriesAndPoint();
+                if (seriesSelection == null) {
+                    Toast.makeText(GraphView.this, "Пусто", Toast.LENGTH_SHORT)
+                            .show();
+                } else {
+
+                    Toast.makeText(
+                            GraphView.this,
+                            " №=" + ((int) seriesSelection.getXValue()) + " : Volt=" + seriesSelection.getValue()
+                            , Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        View.OnClickListener oclBtnGr = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        };
+        BtnGr.setOnClickListener(oclBtnGr);
+    }
+
+    public void initChart() {
+        mCurrentSeries = new XYSeries("");
+        mDataset.addSeries(mCurrentSeries);
+        XYSeriesRenderer mCurrentRenderer = new XYSeriesRenderer();
+        mCurrentRenderer.setChartValuesTextSize(20);
+        mCurrentRenderer.setColor(Color.BLACK);
+        mCurrentRenderer.setChartValuesTextAlign(Paint.Align.CENTER);
+        mCurrentRenderer.setFillPoints(false);
+        mCurrentRenderer.setLineWidth(5);
+
+        mRenderer.addSeriesRenderer(mCurrentRenderer);
+        mRenderer.setBarSpacing(1);
+        mRenderer.setXLabels(30);
+        mRenderer.setYLabels(30);
+        mRenderer.setXAxisMin(0);
+        mRenderer.setXAxisMax(60);
+        mRenderer.setYAxisMin(2);
+        mRenderer.setYAxisMax(4.5);
+        mRenderer.setLabelsTextSize(30);
+        mRenderer.setYLabelsColor(0, Color.RED);
+        mRenderer.setXLabelsColor(Color.RED);
+        mRenderer.setYAxisAlign(Paint.Align.LEFT, 0);
+        mRenderer.setYLabelsAlign(Paint.Align.RIGHT, 0);
+        mRenderer.setShowGridY(true);
+        mRenderer.setShowGridX(true);
+        mRenderer.setApplyBackgroundColor(true);
+        mRenderer.setMarginsColor(Color.WHITE);
+        mRenderer.setMargins(new int[]{0, 50, 0, 50});
+        mRenderer.setBarSpacing(0.15);
+        mRenderer.setPanLimits(new double[]{0, 160, 2, 5});
+        mRenderer.setZoomLimits(new double[]{0, 160, 2, 5});
+        mRenderer.setClickEnabled(true);
+        mRenderer.setShowLegend(false);
+        mRenderer.setBarWidth(30);
+    }
+
+    public void addSampleData() {
+        runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                for (int i = 1; i < 160; i++)
+                    mCurrentSeries.add(ArrayBattery[i][0], ArrayBattery[i][1]);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/vetal/bluetoothdeb/MainActivity.java
@@ -0,0 +1,379 @@
+package com.vetal.bluetoothdeb;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
+
+import static android.R.layout.simple_list_item_1;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final int MAX_IDX_BUFF_FIFO = 100;
+    private static final int MAX_IDX_BUFF_MES = 8;
+    private static final int MAX_IDX_ARAYBATTERY = 160;
+    private UUID myUUID;
+
+    public short[] Buffer_FIFO = new short[MAX_IDX_BUFF_FIFO];
+    public short[] Buff_Mes = new short[MAX_IDX_BUFF_MES];
+    protected static float[][] ArrayBattery = new float[MAX_IDX_ARAYBATTERY][MAX_IDX_BUFF_MES];
+    public int Idx_Buf_In, IdxWrite_Buf_Fifo, IdxRead_Buf_Fifo, Idx_Buf_Mes, Size_Buf_In = 0;
+
+    BluetoothAdapter bluetoothAdapter;
+    ArrayList<String> pairedDeviceArrayList;
+    ListView listViewPairedDevice;
+    TextView myTextView;
+    Button BtnScan, BtnClear, BtnStop, BtnGraph;
+    ArrayAdapter<String> pairedDeviceAdapter;
+    ThreadConnectBTdevice myThreadConnectBTdevice;
+    ThreadConnected myThreadConnected;
+    Thread myThreadRead;
+
+    boolean Pause,Pause2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Pause = false;
+        final String UUID_STRING_WELL_KNOWN_SPP = "00001101-0000-1000-8000-00805F9B34FB";
+
+        BtnScan = (Button) findViewById(R.id.btnScan);
+        BtnClear = (Button) findViewById(R.id.btnClear);
+        BtnStop = (Button) findViewById(R.id.btnStop);
+        BtnGraph = (Button) findViewById(R.id.btnGraph);
+        listViewPairedDevice = (ListView) findViewById(R.id.pairedlist);
+        myTextView = (TextView) findViewById(R.id.textView);
+        myTextView.setMovementMethod(new ScrollingMovementMethod());
+
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)) {
+            Toast.makeText(this, "BLUETOOTH NOT support", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        myUUID = UUID.fromString(UUID_STRING_WELL_KNOWN_SPP);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not supported on this hardware platform", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        View.OnClickListener oclBtnScan = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setup();
+            }
+        };
+        View.OnClickListener oclBtnClear = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myTextView.setText("");
+            }
+        };
+        View.OnClickListener oclBtnStop = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!Pause) {
+                    Pause = true;
+                    BtnStop.setText(getString(R.string.play));
+                } else {
+                    Pause = false;
+                    BtnStop.setText(getString(R.string.stop));
+                }
+            }
+        };
+        View.OnClickListener oclBtnGraph = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, GraphView.class));
+                Pause2 = true;
+            }
+        };
+        BtnScan.setOnClickListener(oclBtnScan);
+        BtnClear.setOnClickListener(oclBtnClear);
+        BtnStop.setOnClickListener(oclBtnStop);
+        BtnGraph.setOnClickListener(oclBtnGraph);
+
+    } // END onCreate
+
+    void appendLog(final String Nmb, final String Voltage, final String Tmp) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                StringBuilder msg = new StringBuilder();
+                // Построение строки
+                msg.append("№:").append(Nmb).append(" ").append("U:").append(Voltage).append(" ").append("t:").append(Tmp).append("\n");
+                //  Вывод на экран
+                myTextView.append(msg);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() { // Запрос на включение Bluetooth
+        super.onStart();
+
+    }
+
+    private void setup() { // Создание списка сопряжённых Bluetooth-устройств
+
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+
+        if (pairedDevices.size() > 0) { // Если есть сопряжённые устройства
+
+            pairedDeviceArrayList = new ArrayList<>();
+
+            for (BluetoothDevice device : pairedDevices) { // Добавляем сопряжённые устройства - Имя + MAC-адресс
+                pairedDeviceArrayList.add(device.getName() + "\n" + device.getAddress());
+            }
+
+            pairedDeviceAdapter = new ArrayAdapter<>(this, simple_list_item_1, pairedDeviceArrayList);
+            listViewPairedDevice.setAdapter(pairedDeviceAdapter);
+
+            listViewPairedDevice.setOnItemClickListener(new AdapterView.OnItemClickListener() { // Клик по нужному устройству
+
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                    listViewPairedDevice.setVisibility(View.GONE); // После клика скрываем список
+
+                    String itemValue = (String) listViewPairedDevice.getItemAtPosition(position);
+                    String MAC = itemValue.substring(itemValue.length() - 17); // Вычленяем MAC-адрес
+
+                    BluetoothDevice device2 = bluetoothAdapter.getRemoteDevice(MAC);
+
+                    myThreadConnectBTdevice = new ThreadConnectBTdevice(device2);
+                    myThreadConnectBTdevice.start();  // Запускаем поток для подключения Bluetooth
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onDestroy() { // Закрытие приложения
+        super.onDestroy();
+        if (myThreadConnectBTdevice != null) myThreadConnectBTdevice.cancel();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_ENABLE_BT) { // Если разрешили включить Bluetooth, тогда void setup()
+
+            if (resultCode == Activity.RESULT_OK) {
+                setup();
+            } else { // Если не разрешили, тогда закрываем приложение
+
+                Toast.makeText(this, "BlueTooth не включён", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    // ============================================================================
+    private class ThreadConnectBTdevice extends Thread { // Поток для коннекта с Bluetooth
+
+        private BluetoothSocket bluetoothSocket = null;
+
+        private ThreadConnectBTdevice(BluetoothDevice device) {
+
+            try {
+                bluetoothSocket = device.createRfcommSocketToServiceRecord(myUUID);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        @Override
+        public void run() { // Коннект
+
+            boolean success = false;
+
+            try {
+                bluetoothSocket.connect();
+                success = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "Нет коннекта, проверьте Bluetooth-устройство с которым хотите соединиться!", Toast.LENGTH_LONG).show();
+                        listViewPairedDevice.setVisibility(View.VISIBLE);
+                    }
+                });
+
+                try {
+                    bluetoothSocket.close();
+                } catch (IOException e1) {
+
+                    e1.printStackTrace();
+                }
+            }
+
+            if (success) {  // Если законнектились, тогда открываем панель с кнопками и запускаем поток приёма и отправки данных
+
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        myTextView.setVisibility(View.VISIBLE);
+                    }
+                });
+
+                myThreadConnected = new ThreadConnected(bluetoothSocket);
+                myThreadConnected.start(); // запуск потока приёма данных
+                //Создание потока
+                myThreadRead = new ReadData();
+                myThreadRead.start();
+            }
+        }
+
+
+        public void cancel() {
+
+            Toast.makeText(getApplicationContext(), "Close - BluetoothSocket", Toast.LENGTH_LONG).show();
+
+            try {
+                bluetoothSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+    private class ThreadConnected extends Thread {    // Поток - приём  данных
+
+        private final InputStream connectedInputStream;
+
+        private ThreadConnected(BluetoothSocket socket) {
+
+            InputStream in = null;
+            try {
+                in = socket.getInputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            connectedInputStream = in;
+        }
+
+        @Override
+        public void run() { // Приём данных
+
+            while (true) {
+                Size_Buf_In = 0;
+                byte[] buffer = new byte[100];
+                try {
+                    int data = connectedInputStream.read(buffer);
+                    WriteData(buffer, data);
+                } catch (IOException e) {
+                    break;
+                }
+            }
+
+        }
+
+        private void WriteData(byte[] b, int Size_Buf_In) {
+            Idx_Buf_In = 0;
+            do {
+                Buffer_FIFO[IdxWrite_Buf_Fifo] = (b[Idx_Buf_In] < 0) ? ((short) ((b[Idx_Buf_In] & 127) | 128)) : ((short) (b[Idx_Buf_In]));// Переводим signet байт в short и записываем в кольцевой массив
+                Idx_Buf_In++;
+                IdxWrite_Buf_Fifo++;
+                if (IdxWrite_Buf_Fifo == MAX_IDX_BUFF_FIFO)
+                    IdxWrite_Buf_Fifo = 0;
+                if (IdxWrite_Buf_Fifo == IdxRead_Buf_Fifo) {
+                    Toast.makeText(MainActivity.this, "Ошибка!!!индекс считывания равен индексу записи!", Toast.LENGTH_LONG).show();
+                }
+            }
+            while (Idx_Buf_In < Size_Buf_In);
+        }
+    }
+
+    private class ReadData extends Thread {
+        @Override
+        public void run() {
+            while (true) {
+                while (IdxRead_Buf_Fifo == IdxWrite_Buf_Fifo) {
+                    Toast.makeText(MainActivity.this, "Ждем Данные", Toast.LENGTH_LONG).show(); //Ловушка
+                }
+                switch (Idx_Buf_Mes) {
+                    case 0: {
+                        if (Buffer_FIFO[IdxRead_Buf_Fifo] == 0xaa) {
+                            Idx_Buf_Mes = 1;
+                        } else Idx_Buf_Mes = 0;
+                    }
+                    //Проверка первого байта
+                    break;
+                    case 1: {
+                        if (Buffer_FIFO[IdxRead_Buf_Fifo] == 0x55) {
+                            Idx_Buf_Mes = 2;
+                        } else Idx_Buf_Mes = 0;
+                    }
+                    //Проверка второго байта
+                    break;
+                    case 7: {
+                        Buff_Mes[Idx_Buf_Mes] = Buffer_FIFO[IdxRead_Buf_Fifo];
+                        Idx_Buf_Mes = 0;
+                        if (!Pause) {
+                            appendLog(String.valueOf(Buff_Mes[3]), String.valueOf(ParsingVoltage(Buff_Mes[4])), String.valueOf(ParsingTmp(Buff_Mes[5]))); //Отправка данных
+                        }
+                        ArrayBattery[Buff_Mes[3]][0] = Buff_Mes[3];
+                        ArrayBattery[Buff_Mes[3]][1] = (float) ParsingVoltage(Buff_Mes[4]);
+                        ArrayBattery[Buff_Mes[3]][2] = ParsingTmp(Buff_Mes[5]);
+                        //Запись данных в массив
+                    }
+                    break;
+                    default: {
+                        if (Idx_Buf_Mes >= 2 && Idx_Buf_Mes < 7) {
+                            Buff_Mes[Idx_Buf_Mes] = Buffer_FIFO[IdxRead_Buf_Fifo]; // запись байтов после проверки
+                            Idx_Buf_Mes++;
+                        }
+                    }
+                    break;
+                }
+                IdxRead_Buf_Fifo++;
+                if (IdxRead_Buf_Fifo == MAX_IDX_BUFF_FIFO)
+                    IdxRead_Buf_Fifo = 0;
+                // Обнуление индекса считывания
+            }
+        }
+    }
+
+    public static double ParsingVoltage(short Voltage) {
+
+        return (Math.round((float) (Voltage * 1.07386 + 200))) / 100.;// Формула получения напряжения
+    }
+
+    public static short ParsingTmp(short Tmp) {
+
+        return (short) (Math.abs(Tmp) - 82);// Формула получения темпрературы
+    }
+} // END

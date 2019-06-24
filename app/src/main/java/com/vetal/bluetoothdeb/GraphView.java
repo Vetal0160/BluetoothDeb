a75e39037af050704a1a0c1ea682f6befd6aaa61
Vitali Rabota <vermisco@gmail.com>
2019-06-24 10:34:12 +0300

    added achartengine

--- app/src/main/java/com/vetal/bluetoothdeb/GraphView.java
+++ app/src/main/java/com/vetal/bluetoothdeb/GraphView.java
@@ -27,6 +27,9 @@ public class GraphView extends MainActivity {
     LinearLayout layout;
     Button BtnGr;
     Thread myThreadGraph;
+    XYSeriesRenderer mCurrentRenderer, mCurrentRenderer2;
+    public double[] mass = {3.5,3.4,3.6,3.2,3.9,3.3};
+    float med = 0;
 
 
     @Override
@@ -41,6 +44,7 @@ public class GraphView extends MainActivity {
         BtnGr = (Button) findViewById(R.id.Back);
         if (mChart == null) {
                 initChart();
+                initChart2();
            // addSampleData();
             mChart = ChartFactory.getBarChartView(this, mDataset, mRenderer, BarChart.Type.DEFAULT);
             layout.addView(mChart);
@@ -74,46 +78,23 @@ public class GraphView extends MainActivity {
         BtnGr.setOnClickListener(oclBtnGr);
     }
         public void initChart() {
-            mRenderer.removeAllRenderers();
-            mDataset.clear();
             mCurrentSeries = new XYSeries("");
             mDataset.addSeries(mCurrentSeries);
-            XYSeriesRenderer mCurrentRenderer = new XYSeriesRenderer();
+            mCurrentRenderer = new XYSeriesRenderer();
             mCurrentRenderer.setChartValuesTextSize(20);
-            /*mCurrentRenderer.setColor(Color.BLACK);*/
+            mCurrentRenderer.setColor(Color.BLACK);
             mCurrentRenderer.setChartValuesTextAlign(Paint.Align.CENTER);
             mCurrentRenderer.setFillPoints(false);
             mCurrentRenderer.setLineWidth(5);
 
-          /*  mRenderer.getSeriesRendererAt(0).setGradientEnabled(true);
-            mRenderer.getSeriesRendererAt(0).setGradientStart(0, Color.rgb(025, 117, 025));
-            mRenderer.getSeriesRendererAt(0).setGradientStop(3.5, Color.rgb(94, 158, 94));*/
-            mRenderer.addSeriesRenderer(mCurrentRenderer);
-            mRenderer.setBarSpacing(1);
-            mRenderer.setXLabels(30);
-            mRenderer.setYLabels(30);
-            mRenderer.setXAxisMin(0);
-            mRenderer.setXAxisMax(60);
-            mRenderer.setYAxisMin(2);
-            mRenderer.setYAxisMax(4.5);
-            mRenderer.setLabelsTextSize(30);
-            mRenderer.setYLabelsColor(0, Color.RED);
-            mRenderer.setXLabelsColor(Color.RED);
-            mRenderer.setYAxisAlign(Paint.Align.LEFT, 0);
-            mRenderer.setYLabelsAlign(Paint.Align.RIGHT, 0);
-            mRenderer.setShowGridY(true);
-            mRenderer.setShowGridX(true);
-            mRenderer.setApplyBackgroundColor(true);
-            mRenderer.setMarginsColor(Color.WHITE);
-            mRenderer.setMargins(new int[]{0, 50, 0, 50});
-            mRenderer.setBarSpacing(0.15);
-            mRenderer.setPanLimits(new double[]{0, 160, 2, 5});
-            mRenderer.setZoomLimits(new double[]{0, 160, 2, 5});
-            mRenderer.setClickEnabled(true);
-            mRenderer.setShowLegend(false);
-            mRenderer.setBarWidth(30);
+          /*  mCurrentRenderer2 = new XYSeriesRenderer();
+            mCurrentRenderer2.setChartValuesTextSize(20);
+            mCurrentRenderer2.setColor(Color.RED);
+            mCurrentRenderer2.setChartValuesTextAlign(Paint.Align.CENTER);
+            mCurrentRenderer2.setFillPoints(false);
+            mCurrentRenderer2.setLineWidth(5);*/
 
-                if (ArrayBattery[1][1] != 0) {
+           /*   *//*  if (ArrayBattery[1][1] != 0) {
                     if (ArrayBattery[1][1] <= 3.5) {
                         mCurrentRenderer.setColor(Color.BLACK);
                         mCurrentSeries.add(ArrayBattery[1][0]+1, ArrayBattery[1][1]);
@@ -126,19 +107,83 @@ public class GraphView extends MainActivity {
                         if (mChart != null) {
                             mChart.repaint();//обновление граф.
                         }
-                    }
-            }try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                //Error
+                    }*//*
+            }*/
+        }
+    public void initChart2() {
+        mRenderer.addSeriesRenderer(mCurrentRenderer);
+     //   mRenderer.addSeriesRenderer(mCurrentRenderer2);
+        mRenderer.setBarSpacing(1);
+        mRenderer.setXLabels(30);
+        mRenderer.setYLabels(30);
+        mRenderer.setXAxisMin(0);
+        mRenderer.setXAxisMax(60);
+        mRenderer.setYAxisMin(2);
+        mRenderer.setYAxisMax(4.5);
+        mRenderer.setLabelsTextSize(30);
+        mRenderer.setYLabelsColor(0, Color.RED);
+        mRenderer.setXLabelsColor(Color.RED);
+        mRenderer.setYAxisAlign(Paint.Align.LEFT, 0);
+        mRenderer.setYLabelsAlign(Paint.Align.RIGHT, 0);
+        mRenderer.setShowGridY(true);
+        mRenderer.setShowGridX(true);
+        mRenderer.setApplyBackgroundColor(true);
+        mRenderer.setMarginsColor(Color.WHITE);
+        mRenderer.setMargins(new int[]{0, 50, 0, 50});
+        mRenderer.setPanLimits(new double[]{0, 160, 2, 5});
+        mRenderer.setZoomLimits(new double[]{0, 160, 2, 5});
+        mRenderer.setClickEnabled(true);
+        mRenderer.setShowLegend(false);
+        mRenderer.setBarWidth(30);
+       // mRenderer.getSeriesRendererAt(0).setGradientEnabled(true);
+    }
+    public void initColor(int number) {
+        if (mass[number] != 0) {
+            if (mass[number] <= med/6) {
+             //   initChart2();
+                mCurrentRenderer.setColor(Color.BLACK);
+              //  mRenderer.getSeriesRendererAt(0).setGradientStart(med/6, Color.BLACK);
+              //  mRenderer.getSeriesRendererAt(0).setGradientStop(med/6, Color.BLUE);
+                mCurrentSeries.add(number, mass[number]);
+                if (mChart != null) {
+                    mChart.repaint();//обновление граф.
+                }
+            } else {
+              //  initChart2();
+              //  mCurrentRenderer.setColor(Color.RED);
+               // mRenderer.getSeriesRendererAt(0).setGradientStart(med/6, Color.GREEN);
+               // mRenderer.getSeriesRendererAt(0).setGradientStop(med/6, Color.RED);
+                mCurrentSeries.add(number, mass[number]);
+                if (mChart != null) {
+                    mChart.repaint();//обновление граф.
+                }
             }
         }
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            //Error
+        }
+    }
+    public float GetMed(double data[]){
+        for (int i = 0; i<mass.length; i++){
+            med += data[i];
+        }
+        return med;
+    }
   class Graph extends Thread {
       public void run() {
           while (true) {
-              for (int i = 1; i < 5; i++) {
+              GetMed(mass);
+              for (int  i =0; i<6; i++) {
+                  initChart2();
+                  initColor(i);
+              }
+              med = 0;
+              if (mDataset != null) {
+              mDataset.clear();
               initChart();
-          }
+              }
           }
       }
   }
